c325b9d48fcf0fd66ac2b32161395f04e4dd6358
Carol Chung <cch5ng@gmail.com>
2020-08-14 12:14:39 -0700

    stub out init app state definitions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Buttons from './components/Buttons';
 import ShapeEditor from './components/ShapeEditor';
 
+const defaultCircle = {
+  x: 250,
+  y: 250,
+  radius: 25,
+  color: '#000000'
+};
+const defaultRectangle = {
+  x: 250,
+  y: 250,
+  width: 75,
+  height: 50,
+  color: '#000000'
+}
+
 function App() {
+  const [shapes, setShapes] = useState({});
+  const [highlightedShapes, setHighlightedShapes] = useState([]);
+  const [selectedShapes, setSelectedShapes] = useState([]);
+  const [latestShapeId, setLatestShapeId] = useState(1);
+
+  const buildCanvas = () => {
+
+  }
+
+  const getShapesAr = () => {
+    let shapesAr = [];
+    Object.keys(shapes).forEach(k => {
+      shapesAr.push(shapes[k]);
+    });
+    return shapesAr;
+  }
+
+  useEffect(() => {
+    buildCanvas();
+  }, getShapesAr());
+
   return (
     <div>
       <main>
         <Buttons></Buttons>
-        <canvas width='500' height='500' />
+        <canvas width='500' height='500' id='shape-canvas'/>
         <ShapeEditor></ShapeEditor>
       </main>
     </div>
